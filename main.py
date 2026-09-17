f683a6147e3f38167715ee3957a7a5b2d887887c
nahar288 <mmhossainmollah@gmail.com>
2026-09-17 15:15:12 +0600

    made a loop

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ import pandas as pd
 
 headers = {"User-Agent": "Mozilla/5.0"}
 all_data = []
+for i in range(1, 6):
+
+    url = f"https://www.scrapethissite.com/pages/forms/?page_num={i}"
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
+    rows = soup.find_all("tr", class_="team")
+
+    
