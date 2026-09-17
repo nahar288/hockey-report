9b59094a095aa7998daa84940fb00af92cab144a
nahar288 <mmhossainmollah@gmail.com>
2026-09-17 15:16:39 +0600

    scraped table rows

--- main.py
+++ main.py
@@ -11,4 +11,12 @@ for i in range(1, 6):
     soup = BeautifulSoup(response.text, "html.parser")
     rows = soup.find_all("tr", class_="team")
 
-    
+    for r in rows:
+        team = r.find("td", class_="name")
+        year = r.find("td", class_="year")
+        wins = r.find("td", class_="wins")
+        losses = r.find("td", class_="losses")
+        ot_losses = r.find("td", class_="ot-losses")
+        win_pct = r.find("td", class_="pct")
+        goals_for = r.find("td", class_="gf")
+        goals_against = r.find("td", class_="ga")
